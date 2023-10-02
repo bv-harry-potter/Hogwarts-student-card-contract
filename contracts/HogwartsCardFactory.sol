0e17bf30d40de8be724dd081168c32dc38c11d98
Riudiu <lacvert13@gmail.com>
2023-10-02 22:34:41 +0900

    Update HogwartsCardFactory contract

--- contracts/HogwartsCardFactory.sol
+++ contracts/HogwartsCardFactory.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HogwartsCardFactory is ERC721Enumerable, Ownable {
+contract HogwartsCardFactory is ERC721 {
    
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
+    uint256 public mintingFee = 0.01 ether;
 
     constructor() ERC721("HogwartsCardNFT", "HogwartsCard") {}
 
@@ -33,15 +33,15 @@ contract HogwartsCardFactory is ERC721Enumerable, Ownable {
         _;
     }
 
-    mapping(address  => HogwartsCardInfo ) private _infos; //issuer가 발급한 명함 정보
-    mapping(address => uint[]) private _tokenIdsMadeByIssuer;  //issuer가 발급한 명함의 tokenId들
-    mapping(address => mapping(uint=> bool)) private _isTokenStillOwnedByIssuer; //issuer가 발급한 tokenId들이 현재 issuer에게 있는지. 있으면 true, 없으면 false
-    mapping(uint => address) private _issuerOfToken; //tokenId의 issuer
-    mapping(address => uint) private _amountOfTokenOwnedByIssuer; //issuer가 현재 가지고 있는 자신의 명함 개수(발급한 양 - 남들에게 transfer한 양) //ERC721의 _balances는 자신의 명함 개수 뿐만 아니라 자신이 받은 명함 개수까지 value값으로 가진다는 점에서 이 mapping과 차이점을 가짐. 
+mapping(address  => HogwartsCardInfo) private _infos;                 // issuer가 발급한 카드 정보
+    mapping(uint => address) private _issuerOfToken;                  // tokenId의 issuer
+    mapping(address => uint) private _amountOfTokenOwnedByIssuer;     // issuer가 현재 가지고 있는 자신의 카드 개수(발급한 양 - 남들에게 transfer한 양
+                                                                      // ERC721의 _balances는 자신의 카드 개수 뿐만 아니라 자신이 받은 카드 개수까지 value값으로 가진다는 점에서 이 mapping과 차이점을 가진다 
 
-     //Events
-    event HogwartsCardInfoRegistered(
-        address indexed issuer,
+    //Events
+    event HogwartsCardMinted(
+        uint indexed tokenId,
+        address issuer,
         string name,       
         string age,        
         string description,
@@ -51,14 +51,9 @@ contract HogwartsCardFactory is ERC721Enumerable, Ownable {
         string blood,      
         string dormitory
     );
-    event HogwartsCardMinted(
-        uint indexed tokenId,
-        address issuer,
-        uint amountOfTokenOwnedByIssuer
-    );
 
     //Functions
-    function registerHogwartsCardInfo (
+    function mintHogwartsCard (
         string memory _name,
         string memory _age,
         string memory _description,
@@ -67,7 +62,8 @@ contract HogwartsCardFactory is ERC721Enumerable, Ownable {
         string memory _level,
         string memory _blood,
         string memory _dormitory
-    ) public {
+    ) public payable isHogwartsCardInfoRegistered returns (uint256) { //호그와트 NFT 한 개 발급  
+        // card 정보 세팅
         HogwartsCardInfo memory hogwartsCardInfo = HogwartsCardInfo({
             issuer: msg.sender,
             name: _name,
@@ -79,26 +75,20 @@ contract HogwartsCardFactory is ERC721Enumerable, Ownable {
             blood: _blood,
             dormitory: _dormitory
         });
-               
         _infos[msg.sender] = hogwartsCardInfo;
 
-        emit HogwartsCardInfoRegistered(msg.sender, _name, _age, _description, _mbti, _hobby, _level, _blood, _dormitory);
-    } 
+        // 기존에 민팅한 card가 있는지 여부, 첫 1회만 무료
+        if (_amountOfTokenOwnedByIssuer[msg.sender] >= 1) {
+            require(msg.value == mintingFee);  // 민팅 비용 확인
+        }
 
-    function mintHogwartsCard () public payable isHogwartsCardInfoRegistered returns (uint256) { //호그와트 NFT 한 개 발급      
-        uint256 newTokenId = _incrementTokenId();
-        
+        uint256 newTokenId = _incrementTokenId(); 
+
         _mint(msg.sender, newTokenId);
- 
-        //tokenIds 관련 매핑 업데이트
-        uint[] storage tokenIdsMadeByIssuer = _tokenIdsMadeByIssuer[msg.sender];
-        tokenIdsMadeByIssuer.push(newTokenId);
-        
-        _isTokenStillOwnedByIssuer[msg.sender][newTokenId] = true;
         _issuerOfToken[newTokenId] = msg.sender;      
         _amountOfTokenOwnedByIssuer[msg.sender]++;
 
-        emit HogwartsCardMinted(newTokenId,msg.sender, _amountOfTokenOwnedByIssuer[msg.sender]);
+        emit HogwartsCardMinted(newTokenId, msg.sender, _name, _age, _description, _mbti, _hobby, _level, _blood, _dormitory);
         return newTokenId;
     }
 
